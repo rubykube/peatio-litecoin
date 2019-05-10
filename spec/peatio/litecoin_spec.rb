fecf1dc60eeb32358973b00e86917bcd6774e733
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-05-10 18:39:50 +0300

    Litecoin::Client specs

--- spec/peatio/litecoin_spec.rb
+++ spec/peatio/litecoin_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Peatio::Litecoin do
   it "has a version number" do
     expect(Peatio::Litecoin::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require "bundler/setup"
 require "peatio/litecoin"
+require "pry-byebug"
+require "webmock/rspec"
+require "mocha"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -7,6 +10,8 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  config.mock_with :mocha
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
